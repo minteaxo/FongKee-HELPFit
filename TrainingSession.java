93891271b3f0921456d901fc5f28c113936675be
minteaxo <minteaxo@gmail.com>
2017-09-26 14:34:47 +0800

    Login Interface
    Half done

---
+++ TrainingSession.java
@@ -0,0 +1,120 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mint
+ */
+public abstract class TrainingSession {
+    
+    private String sessionID;
+    private String title;
+    private Date date;
+    private Date time;
+    private double fee;
+    private String status;
+    
+    public TrainingSession(String sessionID, String title, Date date,
+            Date time, double fee, String status){
+        
+        setSessionID(sessionID);
+        setTitle(title);
+        setDate(date);
+        setTime(time);
+        setFee(fee);
+        setStatus(status);
+        
+    }
+
+    /**
+     * @return the sessionID
+     */
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    /**
+     * @param sessionID the sessionID to set
+     */
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the time
+     */
+    public Date getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the fee
+     */
+    public double getFee() {
+        return fee;
+    }
+
+    /**
+     * @param fee the fee to set
+     */
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
